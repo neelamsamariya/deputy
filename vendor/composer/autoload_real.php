f6ab78a6a46e0d1d01f06bcedc1766c559bc62f2
Neelam Thakor <nitsy85@gmail.com>
2022-11-30 19:28:12 +1100

    updated composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitedc94fb114a2d0b2dd31b1ac4530fee9
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitedc94fb114a2d0b2dd31b1ac4530fee9', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitedc94fb114a2d0b2dd31b1ac4530fee9', 'loadClassLoader'));
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'neelamthakor/deputy',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '43bc84021bf8363654755a1b1b4a4253f075347f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -29,9 +29,9 @@
             'dev_requirement' => true,
         ),
         'neelamthakor/deputy' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '43bc84021bf8363654755a1b1b4a4253f075347f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
